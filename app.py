de6c7158e09767f6cbb7fa0ef5103a2b17a3fb34
Isaiah Isaac Bellanton <ibell8@Isaiahs-MacBook-Air.local>
2022-03-28 14:02:52 -0400

    initials

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 app = flask.Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 math_solution = 2+2
 
@@ -11,4 +12,8 @@ def index():
     return flask.render_template("index.html", solution=math_solution)
 
 
-app.run()
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+)
